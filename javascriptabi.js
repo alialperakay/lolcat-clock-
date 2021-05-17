67392fb224e3956cd4cbf6993239b5903f3bb93e
alialperakay <75796001+alialperakay@users.noreply.github.com>
2021-05-17 22:56:24 +0300

    update// still learning
    still learning

--- javascriptabi.js
+++ javascriptabi.js
@@ -1,3 +1,11 @@
+var wakeup=9;
+var lunch=14;
+var nap=15;
+var noon=00;
+var afternoon=12;
+
+
+
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -12,4 +20,29 @@ function startTime() {
   function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
+  }
+
+  var updateClock=function(){
+    var lolcatImage=document.getElementById('lolCat');
+    var image="https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
+    var message= document.getElementById('timeEvent');
+  
+    if(startTime==wakeup){
+      image="https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+      messageText="IZ TIME TO GETUPP";
+    }
+
+    else if(startTime=nap){
+      image="https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+      messageText="I NEED SOME SLEEP";
+    }
+    else if(startTime==lunch){
+      image="https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
+      messageText="LETZ EAT";
+    }
+    message.innerText=messageText;
+
+
+
+
   }
